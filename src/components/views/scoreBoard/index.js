c078630651b242ce567a5848dc05a0359db65cdb
Jenelle Langford <jenellelangford@Jenelles-Air.home>
2020-06-09 18:53:18 -0500

    scoreboard styling

--- src/components/views/scoreBoard/index.js
+++ src/components/views/scoreBoard/index.js
@@ -1,8 +1,39 @@
 import React, { Component } from "react";
-// import "./styles.css";
+import { Button, Table } from 'react-bootstrap'
+import "./scoreBoard.css";
 
 export default class index extends Component {
   render() {
-    return <div>Scores!</div>;
+    return (
+    <div>
+      <div className="scoreboard-container">
+        <div class="row">
+          <p className="scoreboardParagraph">SCOREBOARD</p>
+        </div>
+          
+          <div class="row">
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Score</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td></td>
+                <td></td>
+              </tr>
+            </tbody>
+          </Table>
+          </div>
+
+        <div class="row">
+          <Button>NEW GAME</Button>
+        </div>
+
+        </div>
+      </div>
+    );
   }
 }
