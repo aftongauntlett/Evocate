f3506c87d7112561eb8fb8ed38772482e6d8dd7d
Afton <59119773+aftongauntlett@users.noreply.github.com>
2020-06-08 20:19:29 -0400

    adding react router

--- src/components/board/index.js
+++ src/components/board/index.js
@@ -1,29 +1,28 @@
-import React from "react"
-import PropTypes from "prop-types"
+import React from "react";
+import PropTypes from "prop-types";
 
-import Card from "../Card"
+import Card from "../Card";
 
-export default function Board ({cards, flipped, handleClick}){
-    return (
-    <div className = "board">
-        {cards.map(card => (
-            <Card
-                key={card.id}
-                id={card.id}
-                type={card.type}
-                width={100}
-                height={100}
-                flipped={flipped.includes(card.id)}
-                handleClick={() => handleClick(card.id)}
-              />
-         ))}
-        
+export default function Board({ cards, flipped, handleClick }) {
+  return (
+    <div className="board d-flex flex-wrap">
+      {cards.map((card) => (
+        <Card
+          key={card.id}
+          id={card.id}
+          type={card.type}
+          width={100}
+          height={100}
+          flipped={flipped.includes(card.id)}
+          handleClick={() => handleClick(card.id)}
+        />
+      ))}
     </div>
- )
+  );
 }
 
 Board.propTypes = {
-    cards: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
-    flipped: PropTypes.arrayOf(PropTypes.number).isRequired,
-    handleClick: PropTypes.func.isRequired,
-}
+  cards: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
+  flipped: PropTypes.arrayOf(PropTypes.number).isRequired,
+  handleClick: PropTypes.func.isRequired,
+};
--- src/components/views/home/index.js
+++ src/components/views/home/index.js
@@ -3,6 +3,6 @@ import React, { Component } from "react";
 
 export default class index extends Component {
   render() {
-    return <div>Navbar</div>;
+    return <div>"Home Page"</div>;
   }
 }
