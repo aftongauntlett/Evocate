3d67859766cf14d6644cf0a6d959095336422fd5
Afton <59119773+aftongauntlett@users.noreply.github.com>
2020-06-09 21:58:43 -0400

    styling

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,30 +1,23 @@
 import React, { Component } from "react";
-import {Navbar, Nav} from 'react-bootstrap';
-
-
-// import "./styles.css";
+import { Navbar, Nav } from "react-bootstrap";
+import "./style.css";
 
 export default class index extends Component {
   render() {
     return (
-  
-  <Navbar bg="light" expand="lg">
-    {/* make evocate larger */}
-  <Navbar.Brand>Evocate</Navbar.Brand>
-  <Navbar.Toggle aria-controls="basic-navbar-nav" />
-  <Navbar.Collapse id="basic-navbar-nav">
-    <Nav className="mr-auto">
-      <Nav.Link href="/">Home</Nav.Link>
-      <Nav.Link href="#/scoreBoard">Scoreboard</Nav.Link>
-      <Nav.Link href="#/gameOver">Game Over</Nav.Link>
-      <Nav.Link href="#/gamePage">Main Game Page</Nav.Link>
-
-    </Nav>
-  </Navbar.Collapse>
-</Navbar>
-
+      <Navbar className="color-nav" expand="lg">
+        {/* make evocate larger */}
+        <Navbar.Brand>Evocate</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="#/scoreBoard">Scoreboard</Nav.Link>
+            <Nav.Link href="#/gameOver">Game Over</Nav.Link>
+            <Nav.Link href="#/gamePage">Main Game Page</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
     );
-    
   }
 }
-
