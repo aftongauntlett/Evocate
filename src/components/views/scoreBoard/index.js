f924df85b7d620f96c2bb1adada87097dad81855
Juncong Liang <juncongliang@Juncongs-MBP.fios-router.home>
2020-06-10 01:56:02 -0400

    scoreboard working

--- src/components/views/scoreBoard/index.js
+++ src/components/views/scoreBoard/index.js
@@ -1,8 +1,37 @@
 import React, { Component } from "react";
-// import "./styles.css";
+import { Button } from 'react-bootstrap'
+import "./style.css";
+
+const users = [
+  { username: 'Gary', score: 100 },
+  { username: 'Dan', score: 90 },
+  { username: 'Erik', score: 90},
+]
 
 export default class index extends Component {
-  render() {
-    return <div>Scores!</div>;
+  
+  render () {
+    const usersElements = [] 
+    for (let user of users) {
+      usersElements.push( 
+        <div>
+          <div>Name：{user.username}</div>
+          <div>Score：{user.score}</div>
+          <hr />
+        </div>
+      )
+    }
+
+    return (
+      <div>
+        <div>{usersElements}</div>
+
+        <div class="row">
+          <Button>NEW GAME</Button>
+        </div>
+
+      </div>
+      
+    )
   }
 }
