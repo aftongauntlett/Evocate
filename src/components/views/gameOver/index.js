a9cda467a987e9de1587a097b9aea7148a3b6c33
Afton <59119773+aftongauntlett@users.noreply.github.com>
2020-06-10 01:14:38 -0400

    work in progress

--- src/components/views/gameOver/index.js
+++ src/components/views/gameOver/index.js
@@ -1,38 +1,42 @@
+import axios from "axios";
 import React, { Component } from "react";
-import { Button, Form } from 'react-bootstrap'
+import { Button, Form } from "react-bootstrap";
 import "./style.css";
 
 export default class index extends Component {
+  onSubmit(event) {
+    event.preventDefault();
+    axios.post("http://localhost:8080/api/score", {
+      name: event.currentTarget.name.value,
+    });
+  }
   render() {
     return (
       <div>
-      <div className="game-over-container">
-        <div class="row">
-          <p>GAME OVER</p>
-        </div>
+        <div className="game-over-container">
+          <div className="row">
+            <p>GAME OVER</p>
+          </div>
 
-        <div class="row">
-          <Form>
-          <Form.Control
-          type="text"
-          placeholder="Enter Name"
-          >
-          </Form.Control>
+          <Form onSubmit={this.onSubmit}>
+            <div className="row">
+              <Form.Control
+                type="text"
+                name="name"
+                placeholder="Enter Name"
+              ></Form.Control>
+            </div>
+
+            <div className="row">
+              <Button type="submit">SUBMIT</Button>
+            </div>
+
+            <div className="row">
+              <Button>NEW GAME</Button>
+            </div>
           </Form>
         </div>
-
-        <div class="row">
-          <Button>SUBMIT</Button>
-        </div>
-
-        <div class="row">
-          <Button>NEW GAME</Button>
-        </div>
-
-      
-
       </div>
-    </div>
     );
   }
 }
