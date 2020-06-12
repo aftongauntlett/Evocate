d2a57f821898838dbcfdf80c8fd79ad7f6ce06bb
Mariana Barnett <marianabarnett3@gmail.com>
2020-06-11 22:22:05 -0400

    load themes

--- src/components/views/gamePage/index.js
+++ src/components/views/gamePage/index.js
@@ -9,24 +9,59 @@ import Timer from './timer'
 // import "./styles.css";
 function setupCards() {
   let id = 1
-  const images = [{
-    type: 'react',
-    img: react
-  }, {
-    type: 'cat',
-    img: cat
-  }]
+  function importAll(r) {
+    return r.keys().map(r);
+  }
+
+  const imagesPoke = importAll(require.context('../themes/images/pokemon-images', false, /\.(png|jpe?g|svg)$/));
+  const imagesSuper = importAll(require.context('../themes/images/superhero-images', false, /\.(png|jpe?g|svg)$/));
+
+  const themes = {
+    "default": [{
+      type: 'react',
+      cardImage: react
+    }, {
+      type: 'cat',
+      cardImage: cat
+    }]
+  }
+
+  themes['pokemon'] = Object.keys(imagesPoke).reduce((result, item) => {
+    const getCard = () => ({
+      id: id++,
+      type: item,
+      cardTop,
+      cardImage: imagesPoke[item],
+      flipped: false,
+    })
+    // console.log(getCard())
+    return [...result, getCard()]
+  })
+  id = 0;
+  themes['super'] = Object.keys(imagesSuper).reduce((result, item) => {
+    const getCard = () => ({
+      id: id++,
+      type: item,
+      cardTop,
+      cardImage: imagesSuper[item],
+      flipped: false,
+    })
+    // console.log(getCard())
+    return [...result, getCard()]
+  })
   const cards = []
-  images.forEach(image => {
+  id = 0;
+  themes['super'].forEach(image => {
     let card = {
       id: id,
       type: image.type,
       cardTop,
-      cardImage: image.img,
+      cardImage: image.cardImage,
       flipped: false
     }
     id++;
     cards.push(card)
+    console.log(card)
     card = JSON.parse(JSON.stringify(card))
     card.id = id
     cards.push(card)
@@ -34,6 +69,7 @@ function setupCards() {
   });
   return suffle(cards)
 }
+
 
 function suffle(cardList) {
   let len = cardList.length
@@ -53,7 +89,7 @@ export default class index extends Component {
     const cards = setupCards()
     return (
       <div className="App">
-       <Timer active={true} />
+        <Timer active={true} />
         <Board cards={cards} />
       </div>
     )
