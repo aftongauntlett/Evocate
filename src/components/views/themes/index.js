a6f0e0ebbb932ebf4517f3b477022c36f73b154d
Afton <59119773+aftongauntlett@users.noreply.github.com>
2020-06-11 22:47:22 -0400

    styling

--- src/components/views/themes/index.js
+++ src/components/views/themes/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { Card, Button } from "react-bootstrap";
 import "./themes.css";
 
@@ -10,42 +10,32 @@ export default class index extends Component {
           <div className="row">
             <p className="themeParagraph">CHOOSE YOUR THEME</p>
           </div>
-        
-     <div className="flip-container">
-        <div className="flipper">
-         <div className="row">
-        <Card className="poke-card" >
-            <Card.Body>
-                <Button className="poke-button">Pokemon</Button>
-            </Card.Body>
-        </Card>
-        </div>
-        </div>
-      </div>
 
-      <div className="flip-container">
-        <div className="flipper">
-         <div className="row">
-        <Card className="hero-card" >
-            <Card.Body>
-                <Button className="hero-button">Superheros</Button>
-            </Card.Body>
-        </Card>
-        </div>
-        </div>
-      </div>
-
-      <div className="flip-container">
-        <div className="flipper">
           <div className="row">
-            <Button className="random-button">Random</Button>
+            <Card className="poke-card">
+              <Card.Body>
+                <Button className="poke-button">Pokemon</Button>
+              </Card.Body>
+            </Card>
+          </div>
+
+          <div className="row">
+            <Card className="hero-card">
+              <Card.Body>
+                <Button className="hero-button">Superheroes</Button>
+              </Card.Body>
+            </Card>
+          </div>
+
+          <div className="row">
+            <Card className="random-card">
+              <Card.Body>
+                <Button className="random-button">Random</Button>
+              </Card.Body>
+            </Card>
           </div>
         </div>
       </div>
-
-
-      </div>
-    </div>
     );
   }
 }
