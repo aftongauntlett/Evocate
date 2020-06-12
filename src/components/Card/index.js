75b97b45273c5ae286988d591249c2c54f738a69
Afton <59119773+aftongauntlett@users.noreply.github.com>
2020-06-12 00:57:13 -0400

    Move to rows/columns

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -2,31 +2,24 @@ import React from "react";
 import "./card.css";
 import Flippy, { FrontSide, BackSide } from "react-flippy";
 
-
-const Card = props => {
-  const {cardImage, cardTop, flipped, onClick} = props
+const Card = (props) => {
+  const { cardImage, cardTop, flipped, onClick } = props;
   return (
     <Flippy
-    flipOnHover={false} 
-    isFlipped={flipped}
-  
-    flipDirection="horizontal" 
-    style={{ width: '100px', height: '100px' }}
-  >
-    <FrontSide
-    onClick={onClick}
-  
+      className="my-flippy"
+      flipOnHover={false}
+      isFlipped={flipped}
+      flipDirection="horizontal"
+      style={{ width: "100px", height: "100px" }}
     >
-      <img src={cardTop} alt="main"/>
-    </FrontSide>
-    <BackSide
- 
-      >
-      <img src={cardImage} alt="top"/>
-    </BackSide>
-  </Flippy>
-  
-  )
-}
+      <FrontSide onClick={onClick}>
+        <img src={cardTop} alt="main" />
+      </FrontSide>
+      <BackSide>
+        <img src={cardImage} alt="top" />
+      </BackSide>
+    </Flippy>
+  );
+};
 
 export default Card;
