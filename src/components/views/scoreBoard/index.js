17e8adc2f2cdc3498f6eb126eaec116d7fa5e8a1
Afton <59119773+aftongauntlett@users.noreply.github.com>
2020-06-12 21:03:41 -0400

    touching up styling

--- src/components/views/scoreBoard/index.js
+++ src/components/views/scoreBoard/index.js
@@ -1,44 +1,38 @@
 import React, { Component } from "react";
-import { Button, Table } from 'react-bootstrap'
+import { Button, Table } from "react-bootstrap";
 import "./scoreBoard.css";
 
 export default class index extends Component {
   render() {
     return (
-    <div>
-      <div className="scoreboard-container">
-        <div className="row">
-          <p className="scoreboardParagraph">SCOREBOARD</p>
-        </div>
-          
+      <div>
+        <div className="scoreboard-container">
           <div className="row">
-          <Table striped bordered hover>
-            <thead>
-              <tr>
-                <th>Name</th>
-                <th>Score</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td></td>
-                <td></td>
-              </tr>
-            </tbody>
-          </Table>
+            <p className="scoreboardParagraph">SCOREBOARD</p>
           </div>
-      </div>
 
-
-        <div className="flip-containter">
-          <div className="flipper">
-            <div className="row">
-              <Button href="/">NEW GAME</Button>
-            </div>
+          <div className="row">
+            <Table striped bordered hover>
+              <thead>
+                <tr>
+                  <th className="scoreboardFont">First Name</th>
+                  <th className="scoreboardFont">Score</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td></td>
+                  <td></td>
+                </tr>
+              </tbody>
+            </Table>
           </div>
         </div>
 
-    </div>
+        <div className="row">
+          <Button href="/">NEW GAME</Button>
+        </div>
+      </div>
     );
   }
 }
